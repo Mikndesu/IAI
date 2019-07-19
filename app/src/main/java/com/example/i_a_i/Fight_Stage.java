4167addb9320f888d83cab7927312bf4c1600de7
MitsukiGoto <mi22kknsinge@ezweb.ne.jp>
2019-07-19 19:51:16 +0900

    First Commit

---
+++ app/src/main/java/com/example/i_a_i/Fight_Stage.java
@@ -0,0 +1,52 @@
+package com.example.i_a_i;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Fight_Stage extends AppCompatActivity {
+
+    Handler handler;
+
+    Intent intent = getIntent();
+    int the_time = intent.getIntExtra("Seconds", 0);
+    Random random = new Random();
+    int count = random.nextInt(the_time);
+
+    ImageView fight;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fight__stage);
+
+        fight = findViewById(R.id.fight);
+
+        fight.setVisibility(View.INVISIBLE);
+
+        Timer timer = new Timer(false);
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (count > 0) {
+                            count--;
+                        } else if (count == 0) {
+                            fight.setVisibility(View.VISIBLE);
+                        }
+                    }
+                });
+            }
+        }, 0, 1000);
+
+    }
+}
---
+++ app/src/main/java/com/example/i_a_i/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.i_a_i;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    int second = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void start(View v) {
+        if (second != 0) {
+       Intent intent = new Intent(this, Fight_Stage.class);
+       intent.putExtra("Seconds", second);
+       startActivity(intent);
+        }
+    }
+
+    public void three(View v) {
+        second = 3;
+    }
+
+    public void five(View v) {
+        second = 5;
+    }
+
+    public void ten(View v) {
+        second = 10;
+    }
+}
