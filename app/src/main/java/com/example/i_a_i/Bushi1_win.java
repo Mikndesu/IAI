49a8d51a7870c9a3ed376bd9a05192c0eb53d6cb
MitsukiGoto <mi22kknsinge@ezweb.ne.jp>
2019-07-22 21:55:27 +0900

    Implemented　exit and restart

--- app/src/main/java/com/example/i_a_i/Bushi1_win.java
+++ app/src/main/java/com/example/i_a_i/Bushi1_win.java
@@ -3,6 +3,7 @@ package com.example.i_a_i;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class Bushi1_win extends AppCompatActivity {
@@ -16,6 +17,12 @@ public class Bushi1_win extends AppCompatActivity {
 
         bushi1win = findViewById(R.id.bushi1_win);
         bushi1win.setTextColor(Color.rgb(134, 213, 49));
+    }
+
+    public void exit(View v) {
+    }
+
+    public void re(View v) {
     }
 
 }
