65cf35569803c623c035488b99fcb60b9ea7b792
MitsukiGoto <mi22kknsinge@ezweb.ne.jp>
2019-07-24 00:13:43 +0900

    Complete MainActivity.class

--- app/src/main/java/com/example/i_a_i/MainActivity.java
+++ app/src/main/java/com/example/i_a_i/MainActivity.java
@@ -4,34 +4,62 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    int second = 0;
+//    int second = 0;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//    }
+//
+//    public void start(View v) {
+//        if (second != 0) {
+//            Intent intent = new Intent(this, Fight_Stage.class);
+//            intent.putExtra("Seconds", second);
+//            startActivity(intent);
+//        }
+//    }
+//
+//    public void three(View v) {
+//        second = 3;
+//    }
+//
+//    public void five(View v) {
+//        second = 5;
+//    }
+//
+//    public void ten(View v) {
+//        second = 10;
+//    }
+
+    Button button;
+
+    int seconds;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.fiveseconds);
+                button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                seconds = 5;
+            }
+        });
+
     }
 
     public void start(View v) {
-        if (second != 0) {
-            Intent intent = new Intent(this, Fight_Stage.class);
-            intent.putExtra("Seconds", second);
-            startActivity(intent);
-        }
+        Intent intent = new Intent(this, Fight_Stage.class);
+        intent.putExtra("Second", seconds);
+        startActivity(intent);
     }
 
-    public void three(View v) {
-        second = 3;
-    }
 
-    public void five(View v) {
-        second = 5;
-    }
-
-    public void ten(View v) {
-        second = 10;
-    }
 }
